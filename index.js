736f56af856969581295eec0ccc575c7ca544a69
Md Mehedi Hasan Shanto <ll4146394@gmail.com>
2026-08-24 22:45:35 +0600

    Crrud operation complete

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 
 // mongodb start from hare
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = process.env.MONGODB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,6 +31,60 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // api start from hare
+
+    const db = client.db("tripzen");
+    const destinationCollection = db.collection("destinations");
+
+    // get api
+
+    app.get("/destination", async (req, res) => {
+      const result = await destinationCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/destination/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await destinationCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+
+
+    // patch api
+
+    app.patch("/destination/:id", async (req, res) => {
+      const { id } = req.params;
+      const updatedDestination = req.body;
+      const result = await destinationCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updatedDestination }
+      );
+      res.send(result);
+    })
+
+
+    // delete api
+
+    app.delete("/destination/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await destinationCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+
+
+    // post api
+    app.post("/destination", async (req, res) => {
+      const newDestination = req.body;
+      console.log(newDestination);
+      const result = await destinationCollection.insertOne(newDestination);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -38,7 +92,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
